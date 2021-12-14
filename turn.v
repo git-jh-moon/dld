e0595d59e3571155374a66fe7c58c4235e6e976a
git-jh-moon <82074398+git-jh-moon@users.noreply.github.com>
2021-12-14 23:51:22 +0900

    Update turn.v

--- turn.v
+++ turn.v
@@ -1,31 +1,32 @@
 module turn(
     input   [2:0]   curr_elevator_1, curr_elevator_2,
-    input   [6:0]   up_passenger, down_passenger,
+    input   [13:0]   up_passenger, down_passenger,
     input   [1:0]   dir_elevator,
     input   [5:0]   boarding_1, boarding_2,
-    output wire  [1:0]   turn
+    output wire  [1:0]   turn,
+    output reg  hold_1, hold_2
     );
     
     // flag of remaining passengers above orW beneath
-    wire [6:0] up_1, up_2, down_1, down_2;
+    wire [13:0] up_1, up_2, down_1, down_2;
     rsh up_1_rsh(
         .a(up_passenger),
-        .f(curr_elevator_1),
+        .f(curr_elevator_1 - 1),
         .y(up_1)
         );
     rsh up_2_rsh(
         .a(up_passenger),
-        .f(curr_elevator_2),
+        .f(curr_elevator_2 - 1),
         .y(up_2)
         );
     lsh down_1_lsh(
         .a(down_passenger),
-        .f(curr_elevator_1),
+        .f(curr_elevator_1 - 1),
         .y(down_1)
         );
     lsh down_2_lsh(
         .a(down_passenger),
-        .f(curr_elevator_2),
+        .f(curr_elevator_2 - 1),
         .y(down_2)
         );
     // flag of wheter is elevator is empty
@@ -40,24 +41,34 @@ module turn(
     always @(*) begin
         case(dir_elevator)
             2'b00:  begin
-                turn_1 = empty_1 && (down_1 == 7'b0);
-                turn_2 = empty_2 && (down_2 == 7'b0);
+                turn_1 <= empty_1 && (down_1[11:0] == 12'b0);
+                hold_1 <= (down_1[13:12] > 2'b0)? 1'b1 : 1'b0;
+                turn_2 <= empty_2 && (down_2[11:0] == 12'b0);
+                hold_2 <= (down_2[13:12] > 2'b0)? 1'b1 : 1'b0;
             end
             2'b01:  begin
-                turn_1 = empty_1 && (down_1 == 7'b0);
-                turn_2 = empty_2 && (up_2 == 7'b0);
+                turn_1 <= empty_1 && (down_1[11:0] == 12'b0);
+                hold_1 <= (down_1[13:12] > 2'b0)? 1'b1 : 1'b0;
+                turn_2 <= empty_2 && (up_2[13:2] == 12'b0);
+                hold_2 <= (up_2[1:0] > 2'b0)? 1'b1 : 1'b0;
             end
             2'b10:  begin
-                turn_1 = empty_1 && (up_1 == 7'b0);
-                turn_2 = empty_2 && (down_2 == 7'b0);
+                turn_1 <= empty_1 && (up_1[13:2] == 12'b0);
+                hold_1 <= (up_1[1:0] > 2'b0)? 1'b1 : 1'b0;
+                turn_2 <= empty_2 && (down_2[11:0] == 12'b0);
+                hold_2 <= (down_2[13:12] > 2'b0)? 1'b1 : 1'b0;
             end
             2'b11:  begin
-                turn_1 = empty_1 && (up_1 == 7'b0);
-                turn_2 = empty_2 && (up_2 == 7'b0);
+                turn_1 <= empty_1 && (up_1[13:2] == 12'b0);
+                hold_1 <= (up_1[1:0] > 2'b0)? 1'b1 : 1'b0;
+                turn_2 <= empty_2 && (up_2[13:2] == 12'b0);
+                hold_2 <= (up_2[1:0] > 2'b0)? 1'b1 : 1'b0;
             end
             default:    begin
-                turn_1 = 1'b0;
-                turn_2 = 1'b0;
+                turn_1 <= 1'bx;
+                hold_1 <= 1'bx;
+                turn_2 <= 1'bx;
+                hold_2 <= 1'bx;
             end
         endcase
     end
