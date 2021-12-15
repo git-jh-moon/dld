a9bf2271eacd2d3a5843bd9d51ccceeb0b837402
git-jh-moon <82074398+git-jh-moon@users.noreply.github.com>
2021-12-15 17:04:03 +0900

    Update turn.v

--- turn.v
+++ turn.v
@@ -4,7 +4,7 @@ module turn(
     input   [1:0]   dir_elevator,
     input   [5:0]   boarding_1, boarding_2,
     output wire  [1:0]   turn,
-    output reg   [5:0]   hold_1, hold_2
+    output reg   [6:0]   hold_1, hold_2
     );
     wire [13:0] up_1, up_2, down_1, down_2;
     rsh up_1_rsh(
@@ -19,12 +19,12 @@ module turn(
         );
     lsh down_1_lsh(
         .a(down_passenger),
-        .f(curr_elevator_1 - 1),
+        .f(7-curr_elevator_1),
         .y(down_1)
         );
     lsh down_2_lsh(
         .a(down_passenger),
-        .f(curr_elevator_2 - 1),
+        .f(7-curr_elevator_2),
         .y(down_2)
         );
     // arrival (2bit) - 10 : MSB passenger is arrived, 01 : LSB passenger is arrived, 11 : two passengers are arrived, 00 : no passenger is arrived
@@ -44,6 +44,7 @@ module turn(
     wire [1:0] leaval_2 = (dir_elevator[0])? up_2[1:0] : down_2[13:12];
     wire [1:0] leaval_1_count = leaval_1[1]+leaval_1[0];
     wire [1:0] leaval_2_count = leaval_2[1]+leaval_2[0];
+    wire sim_end = (up_passenger == 14'b0) && (down_passenger == 14'b0) && (boarding_1 == 6'b0) && (boarding_2 == 6'b0);
 
     // target floor of each elevator
     reg turn_1;
@@ -54,8 +55,8 @@ module turn(
     always @(*) begin
         turn_1 <= (dir_elevator[1])? (pos_1 == 2'b0) && (up_1[13:2] == 12'b0) : (pos_1 == 2'b0) && (down_1[11:0] == 12'b0);
         turn_2 <= (dir_elevator[0])? (pos_2 == 2'b0) && (up_2[13:2] == 12'b0) : (pos_2 == 2'b0) && (down_2[11:0] == 12'b0);
-        hold_1 <= {(arrival_1>2'b0), arrival_1, (leaval_1_count < spare_1_count+1) && (leaval_1_count > 2'b0), leaval_1};
-        hold_2 <= {(arrival_2>2'b0), arrival_2, (leaval_2_count < spare_2_count+1) && (leaval_2_count > 2'b0), leaval_2};
+        hold_1 <= {sim_end, (arrival_1>2'b0), spare_1, (leaval_1_count < spare_1_count+1) && (leaval_1_count > 2'b0), leaval_1};
+        hold_2 <= {sim_end, (arrival_2>2'b0), spare_2, (leaval_2_count < spare_2_count+1) && (leaval_2_count > 2'b0), leaval_2};
     end
 	
 endmodule
